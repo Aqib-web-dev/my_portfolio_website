e3ca1e0ab9f094ec68d40ad55128cee17845d1bd
Aqib <ataswwar@jobcase.com>
2023-03-23 01:48:58 +0500

    Updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,12 +15,9 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-          </h1>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]"> Aqib </span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I craft captivating UI/UX experiences - Front-End Maestro.
           </p>
         </div>
       </div>
